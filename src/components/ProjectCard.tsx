80fd2a4d84580b7c6a6241e640fa759e1069504a
myt588 <yetian.mao@gmail.com>
2022-05-05 02:03:47 +0800

    feat: add responsive to center when sp

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,7 +10,7 @@ export default function ProjectCard(props: Props) {
     <a
       href={props.link}
       target="_blank"
-      className="h-full block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      className="mx-auto h-full block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       rel="noreferrer"
     >
       <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
