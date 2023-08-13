eee0329d01a09d74512cad463057a5698e6e48aa
Yetian <yetian.mao@gmail.com>
2023-08-13 16:14:50 +0900

    feat: restart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,16 @@
 import Head from 'next/head';
-import ProjectList from '@/components/ProjectList';
-import AnnualGoals from '@/components/AnnualGoals';
-import Introduction from '@/components/Introduction';
-import Timeline from '@/components/Timeline';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>可能开发的个人主页-圈子们-东京圈-开发圈创始人</title>
-        <meta
-          name="description"
-          content="mytt.dev-可能开发，致力于打造不同人群的圈子来连接起一个个的机会。"
-        />
+        <title>mytt.dev</title>
+        <meta name="description" content="mytt.dev" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="px-2 mx-auto max-w-screen-lg">
-        <h1 className="text-3xl font-bold text-center my-5">可能开发</h1>
-        <h2 className="text-2xl font-bold text-center mb-6">mytt.dev</h2>
-
-        <ProjectList />
-
-        <AnnualGoals />
-
-        <Introduction />
-
-        <Timeline />
+        Empty. Not Sure what to do yet
       </main>
     </div>
   );
